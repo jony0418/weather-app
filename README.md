89a3365763ff458931ee2f63a29a60be893b3253
jony0418 <78985884+jony0418@users.noreply.github.com>
2023-04-25 00:54:32 -0600

    Adding the README.md and the Screenshot.png, also made the comments in the javascript and changes in the color of the css

---
+++ README.md
@@ -0,0 +1,34 @@
+# Weather Dashboard
+
+Weather Dashboard is a web application that allows travelers to check the current weather and 5-day forecast for any city. This application is built using HTML, CSS, and JavaScript, and it fetches data from the OpenWeatherMap API.
+
+![Weather Dashboard Screenshot](./screenshot.png)
+
+## Features
+
+- Search for a city to view its weather information
+- Displays current weather conditions including city name, date, weather icon, temperature, humidity, and wind speed
+- Presents a 5-day forecast with date, weather icon, temperature, wind speed, and humidity
+- Saves search history and allows users to click on a previously searched city to view its weather again
+
+## APIs Used
+
+- [OpenWeatherMap 5 Day Weather Forecast API](https://openweathermap.org/forecast5) for retrieving weather data
+- [OpenWeatherMap Current Weather Data API](https://openweathermap.org/current) for retrieving geographical coordinates given a city name
+
+## Getting Started
+
+1. Clone this repository or download the ZIP file
+2. Open the `index.html` file in your browser
+3. Start searching for cities to view their weather information
+
+## Usage
+
+1. Type a city name into the search input and press enter or click the search button
+2. The current weather and 5-day forecast for the city will be displayed on the screen
+3. Your search history will be saved below the search input
+4. Click on a previously searched city in the search history to view its weather information again
+
+## License
+
+This project is licensed under the MIT License.
--- script.js
+++ script.js
@@ -1,54 +1,65 @@
+//This is the API key that I use to get the data from the API also I use the var to get the elements from the HTML page
 var API_KEY = '91781f1bc46b0ad54c8ff356c93470fd';
 var searchForm = document.getElementById('search-form');
 var searchHistory = document.getElementById('search-history');
 var currentWeather = document.getElementById('current-weather');
 var forecast = document.getElementById('forecast');
-
+//Here I add the event listener to the search form to get the city from the input and use it to get the weather data
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     var city = document.getElementById('city').value;
     getWeatherData(city);
 });
-
+//Here I add the event listener to the search history to get the city from the button and use it to get the weather data
 searchHistory.addEventListener('click', (e) => {
     if (e.target.classList.contains('history-item')) {
         getWeatherData(e.target.textContent);
     }
 });
 
+//Here is the function to get the weather data I use the argument city to get the data from the API 
 function getWeatherData(city) {
     var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    //Here I use the fetch function to get the data from the API
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
+            //Here I use the destructuring to get the data from the API and the coord and name from the data
             var { coord, name } = data;
             var { lat, lon } = coord;
             getCurrentWeather(lat, lon, name)
                 .then((data) => {
+                    //Here i call the function to display the current weather
                     displayCurrentWeather(data, name);
                 })
             getForecast(lat, lon)
                 .then((data) => {
+                    //Here I call the function to display the forecast weather
                     displayForecast(data);
                 })
+                //After that I call the function to add the city to the search history
             addToSearchHistory(name);
         });
 }
-function getCurrentWeather(lat, lon, name) {
+
+//This function is to get the current weather from the API using the lat and lon from the previous function 
+//I exclude the minutely, hourly and alerts because I only need the current weather
+function getCurrentWeather(lat, lon) {
     var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&exclude=minutely,hourly,alerts`;
     return fetch(url).then((response) => response.json());
 }
-
+//In this function I get the forecast weather from the API using the lat and lon from the previous function
+//in the documentation is said that is a 3 hour forecast so I use the filter function to get the data that ends with 12:00:00
 function getForecast(lat, lon) {
     var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     return fetch(url).then((response) => response.json());
 }
-
+//Here I display the current weather using the data from the API and the name from the previous function
 function displayCurrentWeather(data, name) {
     var { dt, temp, humidity, wind_speed, weather } = data.current;
     var date = new Date(dt * 1000);
     var iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}.png`;
-
+//This is the HTML that I use to display the current weather in the page
     currentWeather.innerHTML = `
         <div class="weather-card">
             <h2>${name}</h2>
@@ -60,11 +71,11 @@ function displayCurrentWeather(data, name) {
         </div>
     `;
 }
-
+//Here I display the forecast weather using the data from the API and the name from the previous function
 function displayForecast(data) {
     var { list } = data;
     var dailyData = list.filter((item) => item.dt_txt.endsWith('12:00:00'));
-
+//This is the HTML that I use to display the forecast weather in the page
     forecast.innerHTML = dailyData
         .map((day) => {
             var date = new Date(day.dt * 1000);
@@ -81,7 +92,7 @@ function displayForecast(data) {
         })
         .join('');
 }
-
+//Here I add the city to the search history using the name from the previous function
 function addToSearchHistory(name) {
     var button = document.createElement('button');
     button.textContent = name;
